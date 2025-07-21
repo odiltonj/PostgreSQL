25a614f92c7d7f31f2cd7d5d7405876c5c7e0794
odiltonjr <odiltonjr@live.com>
2025-07-21 15:41:26 -0300

    Update Distinct.sql

--- Distinct.sql
+++ Distinct.sql
@@ -1,3 +1,4 @@
+/*Utilizando a cláusula distinct - para evitar repetições nas consultas */
 select distinct (d.nomedepartamento)
 from funcionarios as f, departamentos as d
 where f.departamento_id = d.id
