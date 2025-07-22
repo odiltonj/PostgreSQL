3a46efd532c00ad46d53c0263233da9b827c3030
odiltonjr <odiltonjr@live.com>
2025-07-22 20:28:55 -0300

    Add files via upload

---
+++ Ordenar_Select.sql
@@ -0,0 +1,10 @@
+select * from funcionarios
+order by nomefuncionario;
+
+
+select * from funcionarios
+order by nomefuncionario desc;
+
+
+select * from funcionarios
+order by nomefuncionario asc, salario desc;
